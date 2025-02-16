a0c2c34ecdfd106ae5ef7fefdd95d4808be186a4
I am ZaKriya. <zohaib751534@gmail.com>
2025-02-16 23:37:15 +0500

    /api route for home

--- API/Server.js
+++ API/Server.js
@@ -9,6 +9,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from the API!" });
+});
+
 app.get("/api/news", async (req, res) => {
   try {
 
