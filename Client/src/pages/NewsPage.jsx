7978d339e1c8582b55fc361b6ca18474f53ed12e
I am ZaKriya. <zohaib751534@gmail.com>
2025-03-13 21:58:02 +0500

    added loading-icon

--- Client/src/pages/NewsPage.jsx
+++ Client/src/pages/NewsPage.jsx
@@ -32,8 +32,15 @@ const NewsPage = () => {
         console.log(post);
     }, []);
 
-    return blogPosts && blogPosts.length === 0 ? <div className="text-2xl font-semibold text-center w-full flex justify-center items-center h-screen">Loading...</div> :
-        <section className="h-auto max-w-[var(--maxWidth)] pt-12 flex items-center flex-col gap-10">
+    return blogPosts && blogPosts.length === 0 ? (
+        <div className="text-2xl font-semibold text-center w-full flex justify-center items-center h-screen">
+          <svg className="animate-spin h-8 w-8 mr-3 text-gray-500" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
+            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z" />
+          </svg>
+          be patient !
+        </div>
+      ) :     <section className="h-auto max-w-[var(--maxWidth)] pt-12 flex items-center flex-col gap-10">
             <header className="text-5xl font-semibold flex flex-col items-center">Latest Articles
                 <div className="text-[1rem] mt-2 font-normal text-gray-400 text-center">Get the latest articles from Dev.to.</div>
             </header>
