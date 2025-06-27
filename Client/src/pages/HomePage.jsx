e2aed3008dd35e472a062b37ca3b5b1db9be0d7a
I am ZaKriya. <zohaib751534@gmail.com>
2025-06-27 16:26:10 +0500

    login with google

--- Client/src/pages/HomePage.jsx
+++ Client/src/pages/HomePage.jsx
@@ -1,8 +1,22 @@
 import { motion } from 'framer-motion'
 import { Link } from 'react-router-dom';
 import Modal from '../components/Modal';
+import { useEffect } from 'react';
+import { toast } from 'react-toastify';
 
 const HomePage = () => {
+
+     useEffect(() => {
+    const tokenGoogle = new URLSearchParams(window.location.search).get(
+      "token"
+    );
+    if (tokenGoogle) {
+      toast.success("Login successful");
+      localStorage.setItem("token", token);
+      navigate("/");
+    }
+    // connectToBackend();
+  }, []);
     return (
         <section className="h-screen max-w-[var(--maxWidth)] pt-32 flex items-center flex-col gap-6">
             <motion.header initial={{ scale: 0.1 }} animate={{ scale: 1 }} transition={{ duration: 0.4 }} className="text-5xl font-semibold px-3 text-center">Your personalized <span className='bg-gradient-to-br from-yellow-300 to-orange-400 bg-clip-text text-transparent font-extrabold text-6xl'>News_Lens</span></motion.header>
