e58d66a40b208eb3e5cd30600408e0a81dfb745f
I am ZaKriya. <zohaib751534@gmail.com>
2025-06-28 00:04:42 +0500

    added token access

--- Client/src/utils/Routing/Routin.jsx
+++ Client/src/utils/Routing/Routin.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Header from '../../components/Header'
 import HomePage from '../../pages/HomePage'
 import News_Detail from '../../pages/News_Detail'
+import { toast } from 'react-toastify';
+
 import NewsPage from '../../pages/NewsPage'
 
 const Routin = () => {
+    useEffect(() => {
+        const tokenGoogle = new URLSearchParams(window.location.search).get(
+            "token"
+        );
+        if (tokenGoogle) {
+            toast.success("Login successful");
+            localStorage.setItem("token", token);
+            navigate("/");
+        }
+    }, []);
+
     return (
         <div className='max-w-5xl mx-auto'>
             <Header />
